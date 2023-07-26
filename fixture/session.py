151ecb011e7bac76ef2ea5ab77d08a39de3f7041
Bramych <Ralivixi@yandex.ru>
2023-07-26 11:14:53 +0300

    add sone new cases

---
+++ fixture/session.py
@@ -0,0 +1,9 @@
+
+
+#class SessionHelper:
+    #def __init__(self):
+    #self.app = app
+
+    #def test_autorization(self, login="9803876814", telcode="0000"):
+    # авторизация юзера
+        #self.open_page()
